b498a48ebb0245b62b5f2f923e22e7439bffff35
CesarSants <cesarsantosplay4@gmail.com>
2025-07-30 22:07:26 -0300

    done10

--- src/mocks/projetos.ts
+++ src/mocks/projetos.ts
@@ -4,6 +4,45 @@ export const projetos = [
     {
         id: 1,
         categoria: 'websites',
+        descricao: 'PcStore é um e-commerce de especializado em computadores e acessórios, possui integração com Google Auth para cadastro e login de usuários, além de integração com gateway de pagamentos.',
+        nome: 'PcStore',
+        link: 'https://pc-store-seven.vercel.app/',
+        gallery: [
+            {
+                url: mediaPath + "/websites/pcStore/PC1.png",
+            },
+            {
+                url: mediaPath + "/websites/pcStore/PC2.png",
+            },
+            {
+                url: mediaPath + "/websites/pcStore/PC3.png",
+            },
+            {
+                url: mediaPath + "/websites/pcStore/PC4.png",
+            },
+            {
+                url: mediaPath + "/websites/pcStore/PC5.png",
+            },
+            {
+                url: mediaPath + "/websites/pcStore/PC6.png",
+            },
+            {
+                url: mediaPath + "/websites/pcStore/PC7.png",
+            },
+            {
+                url: mediaPath + "/websites/pcStore/PC8.png",
+            },
+            {
+                url: mediaPath + "/websites/pcStore/PC9.png",
+            },
+                        {
+                url: mediaPath + "/websites/pcStore/PC10.png",
+            }
+        ]
+    },
+    {
+        id: 2,
+        categoria: 'websites',
         descricao: 'ePlay é uma plataforma fictícia de e-commerce especializada em jogos, oferece uma experiência completa. Desde a escolha dos títulos mais desejados até a finalização da compra.',
         nome: 'ePlay',
         link: 'https://e-play-ashy.vercel.app/',
@@ -38,7 +77,7 @@ export const projetos = [
         ]
     },
     {
-        id: 2,
+        id: 3,
         categoria: 'websites',
         descricao: 'eFood é uma loja fictícia focada em gastronomia. Com um vasto cardápio, oferece uma experiência prática e saborosa, desde a escolha dos pratos até a finalização do pedido.',
         nome: 'eFood',
@@ -74,9 +113,9 @@ export const projetos = [
         ]
     },
     {
-        id: 3,
+        id: 4,
         categoria: 'websites',
-        descricao: 'Esta Landing Page é uma experiência imersiva dedicada ao universo de Star Wars. Descubra todos os filmes e series da franquia e conheça os pacotes para ter acesso ao conteúdo.',
+        descricao: 'Esta Landing Page é uma experiência imersiva dedicada ao universo de Star Wars. Descubra todos os filmes e séries da franquia e conheça os pacotes para ter acesso ao conteúdo.',
         nome: 'StarWars LandingPage',
         link: 'https://site-star-wars-gold.vercel.app/',
         gallery: [
@@ -100,8 +139,41 @@ export const projetos = [
             }
         ]
     },
+        {
+        id: 5,
+        categoria: 'websites',
+        descricao: 'Este é um sistema que simula a interface do totem de pedidos do Mc Donalds, ideal para telas com largura reduzida, além disso possui integração com gateway de pagamentos.',
+        nome: 'Mc Donalds',
+        link: 'https://mac-donalds-fs.vercel.app/donalds',
+        gallery: [
+            {
+                url: mediaPath + "/websites/mac/MC1.png",
+            },
+            {
+                url: mediaPath + "/websites/mac/MC2.png",
+            },
+            {
+                url: mediaPath + "/websites/mac/MC3.png",
+            },
+            {
+                url: mediaPath + "/websites/mac/MC4.png",
+            },
+            {
+                url: mediaPath + "/websites/mac/MC5.png",
+            },
+            {
+                url: mediaPath + "/websites/mac/MC6.png",
+            },
+            {
+                url: mediaPath + "/websites/mac/MC7.png",
+            },
+            {
+                url: mediaPath + "/websites/mac/MC8.png",
+            }
+        ]
+    },
     {
-        id: 4,
+        id: 6,
         categoria: 'websites',
         descricao: 'Travela é um site fictício de viagens projetado para inspirar e facilitar a exploração de destinos ao redor do mundo. Com uma interface intuitiva, design moderno e responsivo.',
         nome: 'Travela',
@@ -119,7 +191,7 @@ export const projetos = [
         ]
     },
     {
-        id: 5,
+        id: 7,
         categoria: 'websites',
         descricao: 'Esta landing page fictícia de um restaurante foi criada para oferecer uma experiência visual atraente e intuitiva, destacando o cardápio, localização e a história do restaurante.',
         nome: 'Restaurante LandingPage',
@@ -137,7 +209,7 @@ export const projetos = [
         ]
     },
     {
-        id: 6,
+        id: 8,
         categoria: 'websites',
         descricao: 'Esta landing page fictícia de uma loja de tênis foi projetada para proporcionar uma experiência de compra online. Com um layout moderno e focado na experiência do usuário.',
         nome: 'Shoes Store',
@@ -152,7 +224,61 @@ export const projetos = [
         ]
     },
     {
-        id: 7,
+        id: 9,
+        categoria: 'ferramentas',
+        descricao: 'Sistema de agendamentos, com verificação de disponibilidade em tempo real. Possui funcionalidades como login, cadastro, hibernação e conclusão de agendamentos automaticamente.',
+        nome: 'Sistema de Agendamentos',
+        link: 'http://137.131.233.103:8080/agendamentos-online/index.xhtml/',
+        gallery: [
+            {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG1.png",
+            },
+            {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG2.png",
+            },
+            {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG3.png",
+            },
+            {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG4.png",
+            },
+            {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG5.png",
+            },
+            {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG6.png",
+            },
+            {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG7.png",
+            },
+            {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG8.png",
+            },
+            {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG9.png",
+            },
+                        {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG10.png",
+            },
+                        {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG11.png",
+            },
+                        {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG12.png",
+            },
+                        {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG13.png",
+            },
+                        {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG14.png",
+            },
+                        {
+                url: mediaPath + "/ferramentas/sistemaAgendamento/AG15.png",
+            }
+        ]
+    },
+    {
+        id: 10,
         categoria: 'ferramentas',
         descricao: 'Esse projeto é uma lista de tarefas, desenvolvida para ajudar os usuários a organizar suas atividades de forma eficiente. possui edição, buscas, filtros, e alteração de status.',
         nome: 'Lista de tarefas',
@@ -167,9 +293,9 @@ export const projetos = [
         ]
     },
     {
-        id: 8,
+        id: 11,
         categoria: 'ferramentas',
-        descricao: 'Este projeto é uma calculadora de IMC (Índice de Massa Corporal). O usuário precisa digitar sua altura e peso e a ferramenta apresenta o resultado de forma dinâmica em uma regua.',
+        descricao: 'Este projeto é uma calculadora de IMC (Índice de Massa Corporal). O usuário precisa digitar sua altura e peso e a ferramenta apresenta o resultado de forma dinâmica em uma régua.',
         nome: 'Calculadora de IMC',
         link: 'https://calcule-imc-rho.vercel.app/',
         gallery: [
@@ -182,7 +308,7 @@ export const projetos = [
         ]
     },
     {
-        id: 9,
+        id: 12,
         categoria: 'ferramentas',
         descricao: 'Essa ferramenta é uma lista de contatos que permite aos usuários gerenciar facilmente suas informações com funcionalidades como filtros personalizados, opção de edição e remoção.',
         nome: 'Lista de contatos',
@@ -197,9 +323,9 @@ export const projetos = [
         ]
     },
     {
-        id: 10,
+        id: 13,
         categoria: 'ferramentas',
-        descricao: 'Essa ferramenta tem como objetivo o facil acesso aos repositorios e informções de qualquer perfil atraves da API do GitHub, basta o usuário digitar o Username e clicar em "buscar".',
+        descricao: 'Essa ferramenta tem como objetivo o facil acesso aos repositorios e informações de qualquer perfil atraves da API do GitHub, basta o usuário digitar o Username e clicar em "buscar".',
         nome: 'GitHub random',
         link: 'https://github-perfil-lemon.vercel.app/',
         gallery: [
@@ -212,7 +338,7 @@ export const projetos = [
         ]
     },
     {
-        id: 11,
+        id: 14,
         categoria: 'ferramentas',
         descricao: 'A ferramenta analisa dados detalhados de contas no GitHub, oferecendo insights sobre contribuições, repositórios, seguidores, atividades e principais tecnologias utilizadas.',
         nome: 'GitHub analizer',
@@ -227,9 +353,9 @@ export const projetos = [
         ]
     },
     {
-        id: 12,
+        id: 15,
         categoria: 'ferramentas',
-        descricao: 'A calculadora oferece uma interface simples e intuitiva, realiza operações de adição, subtração, multiplicação e divisão, alem de permitir salvar e filtrar as operações.',
+        descricao: 'A calculadora oferece uma interface simples e intuitiva, realiza operações de adição, subtração, multiplicação e divisão, além de permitir salvar e filtrar as operações.',
         nome: 'Calculadora',
         link: 'https://calculadora-vue-js-wheat.vercel.app/',
         gallery: [
